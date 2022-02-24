0225ebe2982d911c3675ffd7156d56392a56543e
huhn511 <sehe89@gmail.com>
2022-02-24 13:34:12 +0100

    add buy starter pack functionality

--- src/pages/Collection.tsx
+++ src/pages/Collection.tsx
@@ -9,43 +9,52 @@ import BaseLayout from '../layouts/BaseLayout'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const CARDS_ABI = require('../contracts/cards.json')
 
-const CARDS_ADDRESS = '0x29e755AD70B44d4e729483BDd35115E6f76E221F'
+const CARDS_ADDRESS = '0xEB8210367814725d2F787C3040Eeb18684EA7E09'
 
 const Collection: React.FC = () => {
   const { account, chainId, library } = useWeb3React()
 
   const [contract, setContract] = React.useState(undefined)
 
-
   const init = (_account: any, _library: any): void => {
+    console.log('init')
     const web3 = new Web3(_library.provider)
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    setContract(new web3.eth.Contract(CARDS_ABI, CARDS_ADDRESS))
-  }
-
-  const buy = async (): Promise<void> => {
-    const web3 = new Web3(library.provider)
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     const test = new web3.eth.Contract(CARDS_ABI, CARDS_ADDRESS)
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     setContract(test)
-    const data = await test.methods.buy_starter().call()
-    console.log('data', data)
+  }
+
+  const buy = async (): Promise<void> => {
+    // const web3 = new Web3(library.provider)
+    // // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // // @ts-ignore
+    // const test = new web3.eth.Contract(CARDS_ABI, CARDS_ADDRESS)
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // setContract(test)
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    if (contract.methods) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      const data = await contract.methods.buy_starter().send({ from: account })
+      console.log('data', data)
+    }
   }
 
   React.useEffect(() => {
+    if (!!account && !!library) {
+      init(account, library)
+    }
     console.log('yoyo')
   }, [account, library])
 
   return (
     <BaseLayout>
       <Typography variant='h1'>Collection</Typography>
-      {contract && (
-        <Button onClick={buy}>Buy Booster pack</Button>
-      )}
+      {contract && <Button onClick={buy}>Buy Booster pack</Button>}
     </BaseLayout>
   )
 }
